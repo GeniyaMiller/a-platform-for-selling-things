2659ec639d4cd5ec6c503e02cea37d3097a88b36
Alexust11 <110272504+Alexust11@users.noreply.github.com>
2023-05-28 01:45:21 +0700

    Конгтроллер User

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.service.AuthService;
+import ru.skypro.homework.service.UserService;
 
 @RestController
 @CrossOrigin(value = "http://localhost:3000")
@@ -18,4 +22,12 @@ import org.springframework.web.bind.annotation.*;
         }
 )
 public class UserController {
+        private final UserService userService;
+        private final AuthService authService;
+        private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+        public UserController(UserService userService, AuthService authService) {
+                this.userService = userService;
+                this.authService = authService;
+        }
 }
