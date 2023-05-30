3df11c356c66926c72fcd08f7d89ca0fa596bf72
Alexust11 <110272504+Alexust11@users.noreply.github.com>
2023-05-30 18:59:25 +0700

    подключил БД

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,5 +1,59 @@
 package ru.skypro.homework.controller;
 
+<<<<<<< HEAD
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.ads.Ads;
+import ru.skypro.homework.dto.ads.CreateAds;
+import ru.skypro.homework.dto.ads.FullAds;
+import ru.skypro.homework.dto.ads.ResponseWrapperAds;
+
+@Slf4j
+@RestController
+@RequestMapping("/ads")
+@RequiredArgsConstructor
+@CrossOrigin(value = {"http://localhost:3000", "http://localhost:3000"})
+public class AdsController {
+    @GetMapping()
+    public ResponseEntity<ResponseWrapperAds> getAllAds(@RequestParam(required = false) String title) {
+        return null;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Ads> addAds(@RequestPart("properties") CreateAds properties,
+                                      @RequestPart("image") MultipartFile image){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FullAds> getAds(@PathVariable("id") Integer id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeAd(@PathVariable("id") Integer id) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Ads> updateAds(@PathVariable("id") Integer id,
+                                         @RequestBody CreateAds createAds) {
+        return null;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ResponseWrapperAds> getAdsMe() {
+        return null;
+    }
+
+    @PatchMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<byte[]> updateAdsImage(@PathVariable Integer id, @RequestParam MultipartFile image){
+        return null;
+    }
 =======
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -44,41 +98,8 @@ public class AdsController {
         ResponseWrapperCommentDto comments = new ResponseWrapperCommentDto(adsService.getAdsComments(id));
         return ResponseEntity.ok(comments);
     }
-  
-    @GetMapping()
-    public ResponseEntity<ResponseWrapperAds> getAllAds(@RequestParam(required = false) String title) {
-        return null;
-    }
 
-    @PostMapping()
-    public ResponseEntity<Ads> addAds(@RequestPart("properties") CreateAds properties,
-                                      @RequestPart("image") MultipartFile image){
-        return null;
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<FullAds> getAds(@PathVariable("id") Integer id) {
-        return null;
-    }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> removeAd(@PathVariable("id") Integer id) {
-        return null;
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<Ads> updateAds(@PathVariable("id") Integer id,
-                                         @RequestBody CreateAds createAds) {
-        return null;
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<ResponseWrapperAds> getAdsMe() {
-        return null;
-    }
-
-    @PatchMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<byte[]> updateAdsImage(@PathVariable Integer id, @RequestParam MultipartFile image){
-        return null;
-    }
+>>>>>>> 515974a (Контроллер, сервис по комментариям к объявлениям)
 }
