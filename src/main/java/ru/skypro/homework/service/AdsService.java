8c8bdd40ebcbb09db28401889861f265d6d01e33
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2023-05-31 06:56:10 +0400

    эндпоинты по коментариям

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -1,10 +1,16 @@
 package ru.skypro.homework.service;
 
+import org.springframework.security.core.Authentication;
 import ru.skypro.homework.dto.ads.CommentDto;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.util.Collection;
 
 public interface AdsService {
     Collection<CommentDto> getAdsComments(int adsId);
 
+    CommentDto addComment(Integer id, CommentDto comment, Authentication authentication);
+    void deleteComment(Integer adId, Integer commentId, Authentication authentication);
+
+    CommentDto updateComment(Integer adId, Integer commentId, CommentDto comment, Authentication authentication);
 }
