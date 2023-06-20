0c871901cfaf6a8f014512a90e453604e14566d0
alexust <alexust11@yandex.ru>
2023-06-20 17:27:24 +0700

    перенес кое-что из weeks3

--- src/main/java/ru/skypro/homework/dto/auth/RegReq.java
+++ src/main/java/ru/skypro/homework/dto/auth/RegReq.java
@@ -1,7 +1,8 @@
 package ru.skypro.homework.dto.auth;
 
+import lombok.Data;
 import ru.skypro.homework.dto.profile.Role;
-
+@Data
 public class RegReq {
     private String username;
     private String password;
