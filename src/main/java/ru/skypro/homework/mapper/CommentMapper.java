95cc69b22682e4fc463b4fce449be40cc5dbcd23
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2023-06-23 22:01:03 +0400

    маппер по комментариям

--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -1,17 +1,38 @@
 package ru.skypro.homework.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 import ru.skypro.homework.dto.ads.CommentDto;
 import ru.skypro.homework.model.Comment;
 
-@Mapper(componentModel = "spring")
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface CommentMapper {
 
-
+    @Mapping(target = "id", source = "pk")
+    @Mapping(target = "authorId", ignore = true)
+    @Mapping(target = "adsId", ignore = true)
+    @Mapping(target = "createdAt", source = "createdAt")
     Comment commentDtoToComment(CommentDto commentDto);
 
+    @Mapping(target = "author", source = "authorId.id")
+    @Mapping(target = "authorImage", ignore = true)
+    @Mapping(target = "authorFirstName", ignore = true)
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "pk", source = "id")
     CommentDto commentToCommentDto(Comment comment);
+
+    default LocalDateTime toLocalDateTime(Long longDt) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(longDt), ZoneId.systemDefault());
+    }
+
+    default Long toLongDateTime(LocalDateTime locDt) {
+        return locDt.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
 
 
 
